b675c6ed5e891639bc5df4990e0e4ffbde1e54bd
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2023-07-18 02:31:04 +1200

    deploy to azure

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 async function getUsers() {
-  const res = await fetch('dzmsabackend.azurewebsites.net/api/User');
+  const res = await fetch('https://dzmsabackend.azurewebsites.net/api/User');
   return await res.json()
 }
 
